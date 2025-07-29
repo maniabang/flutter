597651bec37107543c4cfce019aa5c632a9fea2f
maniabang <madmanno@naver.com>
2025-07-29 16:23:36 +0800

    Create Home Page

--- myflight/lib/main.dart
+++ myflight/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'splash_screen.dart';
+import 'home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +32,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: SplashScreen(),
+      home: HomeScreen(),
     );
   }
 }
